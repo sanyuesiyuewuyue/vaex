60de8fde85f7a39d32b0485092a3df0102461720
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-12-26 11:37:33 +0100

    chore: Linting

--- tests/concat_test.py
+++ tests/concat_test.py
@@ -1,18 +1,18 @@
 import vaex
 import numpy as np
-from functools import reduce
+
 
 def test_concat():
-    x1, y1, z1 = np.arange(3), np.arange(3,0,-1), np.arange(10,13)
-    x2, y2, z2 = np.arange(3,6), np.arange(0,-3,-1), np.arange(13,16)
-    x3, y3, z3 = np.arange(6,9), np.arange(-3,-6,-1), np.arange(16,19)
+    x1, y1, z1 = np.arange(3), np.arange(3, 0, -1), np.arange(10, 13)
+    x2, y2, z2 = np.arange(3, 6), np.arange(0, -3, -1), np.arange(13, 16)
+    x3, y3, z3 = np.arange(6, 9), np.arange(-3, -6, -1), np.arange(16, 19)
     w1, w2, w3 = np.array(['cat']*3), np.array(['dog']*3), np.array(['fish']*3)
-    x = np.concatenate((x1,x2,x3))
-    y = np.concatenate((y1,y2,y3))
-    z = np.concatenate((z1,z2,z3))
-    w = np.concatenate((w1,w2,w3))
+    x = np.concatenate((x1, x2, x3))
+    y = np.concatenate((y1, y2, y3))
+    z = np.concatenate((z1, z2, z3))
+    w = np.concatenate((w1, w2, w3))
 
-    ds  = vaex.from_arrays(x=x, y=y, z=z, w=w)
+    ds = vaex.from_arrays(x=x, y=y, z=z, w=w)
     ds1 = vaex.from_arrays(x=x1, y=y1, z=z1, w=w1)
     ds2 = vaex.from_arrays(x=x2, y=y2, z=z2, w=w2)
     ds3 = vaex.from_arrays(x=x3, y=y3, z=z3, w=w3)
@@ -38,6 +38,7 @@ def test_concat():
     assert (np.array(dd2.evaluate('z')) == np.array(ds.evaluate('z'))).all()
     assert (np.array(dd2.evaluate('w')) == np.array(ds.evaluate('w'))).all()
 
+
 def test_concat_unequals_virtual_columns():
     ds1 = vaex.from_scalars(x=1, y=2)
     ds2 = vaex.from_scalars(x=2, y=3)
@@ -51,12 +52,14 @@ def test_concat_unequals_virtual_columns():
     assert ds.w.tolist() == [1+2, 2+3]
     assert ds.z.tolist() == [1+2, 2*3]
 
+
 def test_concat_arrow_strings():
     df1 = vaex.from_arrays(x=vaex.string_column(['aap', 'noot', 'mies']))
     df2 = vaex.from_arrays(x=vaex.string_column(['a', 'b', 'c']))
     df = vaex.concat([df1, df2])
     assert df.dtype('x') == df1.dtype('x')
     assert df.x.tolist() == ['aap', 'noot', 'mies', 'a', 'b', 'c']
+
 
 def test_concat_mixed_types():
     x1 = np.zeros(3) + np.nan
@@ -70,6 +73,7 @@ def test_concat_mixed_types():
     assert df[2:4].x.tolist() == [None, 'hi']
     assert df[3:4].x.tolist() == ['hi']
     assert df[3:5].x.tolist() == ['hi', 'there']
+
 
 def test_concat_missing_values():
     df1 = vaex.from_arrays(x=[1, 2, 3], y=[np.nan, 'b', 'c'])
